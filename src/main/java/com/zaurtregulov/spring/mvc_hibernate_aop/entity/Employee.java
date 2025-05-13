a2f8c4dd5b1b2299b7737c359c27128d558de58a
Вадим <kocichca@bk.ru>
2025-05-13 19:19:20 +0300

    lox

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/Employee.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/entity/Employee.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "employees")
-public class Employee      {
+public class Employee       {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
